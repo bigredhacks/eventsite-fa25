6ee38c2fbfb63d5e6d8d4f510ef9fc25c2365b1f
Tony Oh <thetuner117@gmail.com>
2025-04-28 17:32:58 -0400

    Footer adjustments

--- src/components/ScrollBar/ScrollBarTrack.tsx
+++ src/components/ScrollBar/ScrollBarTrack.tsx
@@ -1,6 +1,6 @@
 const ScrollBarTrack: React.FC = () => {
   return (
-    <div className="absolute top-30 bottom-30 right-14 w-4 bg-purple1 rounded-full z-10 pointer-events-none" />
+    <div className="absolute top-16 bottom-16 right-14 w-4 bg-purple1 rounded-full z-10 pointer-events-none" />
   );
 };
 
--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,37 +1,41 @@
 import { FaInstagram, FaLinkedin, FaEnvelope } from "react-icons/fa";
-import brhLogoWhite from "@/assets/brh_logo_white.png";
+import brhLogoBlack from "@/assets/brh_logo_black.png";
 
 export default function Footer() {
   return (
-    <footer className="w-full bg-gradient-to-b from-purple-600 to-purple-400">
-      <div className="bg-purple-200 flex flex-col md:flex-row justify-between items-center px-24 py-6">
-        <div className="flex flex-col items-start space-y-4">
-          <div className="flex items-center space-x-3">
-            <img src={brhLogoWhite} alt="Big Red Hacks Logo" className="h-16" />
-          </div>
-          <p className="text-lg font-semibold">Made with ❤️ by BigRed//Hacks</p>
-          <p className="text-md">
+    <div
+      className="w-full flex flex-col md:flex-row 
+                 bg-purple6 font-sans font-semibold text-black
+                 justify-between items-center px-36 py-8"
+    >
+      {/* Left Section */}
+      <div className="flex flex-col items-start space-y-4">
+        <img src={brhLogoBlack} alt="Big Red Hacks Logo" className="h-16" />
+        <p className="text-2xl">Made with ❤️ by BigRed//Hacks</p>
+        <div className="flex flex-col space-y-1">
+          <p className="text-lg">
             Registered Student Organization of Cornell University
           </p>
-          <p className="text-md">MLH Code of Conduct</p>
-        </div>
-
-        {/* Right Section */}
-        <div className="flex flex-col items-center space-y-4">
-          <p className="text-lg font-semibold">contact us!</p>
-          <div className="flex space-x-4">
-            <a href="#" aria-label="Instagram">
-              <FaInstagram className="text-2xl" />
-            </a>
-            <a href="#" aria-label="LinkedIn">
-              <FaLinkedin className="text-2xl" />
-            </a>
-            <a href="#" aria-label="Email">
-              <FaEnvelope className="text-2xl" />
-            </a>
-          </div>
+          <p className="text-lg">MLH Code of Conduct</p>
         </div>
       </div>
-    </footer>
+
+      {/* Right Section */}
+      <div className="flex flex-col items-end space-y-4">
+        <p className="text-lg font-semibold">contact us!</p>
+        {/* Logos */}
+        <div className="flex space-x-4 mr-0.5">
+          <a href="#" aria-label="Instagram">
+            <FaInstagram className="text-2xl" />
+          </a>
+          <a href="#" aria-label="LinkedIn">
+            <FaLinkedin className="text-2xl" />
+          </a>
+          <a href="#" aria-label="Email">
+            <FaEnvelope className="text-2xl" />
+          </a>
+        </div>
+      </div>
+    </div>
   );
 }
