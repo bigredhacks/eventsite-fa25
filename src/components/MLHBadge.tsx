9c95532ea1b5b217fb464c84fa77510bfdfea16a
Kelly <ky322@cornell.edu>
2025-06-26 12:52:51 -0600

    Update 2026 Banner

--- src/components/MLHBadge.tsx
+++ src/components/MLHBadge.tsx
@@ -3,14 +3,14 @@ const MLHBadge: React.FC = () => {
     <div className="relative">
       <a
         id="mlh-trust-badge"
-        href="https://mlh.io/na?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2025-season&utm_content=gray"
+        href="https://mlh.io/na?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2026-season&utm_content=gray"
         target="_blank"
         rel="noopener noreferrer"
-        className="hidden md:block fixed right-3.5 w-25 z-200"
+        className="fixed right-3.5 w-[10%] max-w-[100px] min-w-[60px] z-[100]"
       >
         <img
-          src="https://s3.amazonaws.com/logged-assets/trust-badge/2025/mlh-trust-badge-2025-gray.svg"
-          alt="Major League Hacking 2025 Hackathon Season"
+          src="https://s3.amazonaws.com/logged-assets/trust-badge/2026/mlh-trust-badge-2026-gray.svg"
+          alt="Major League Hacking 2026 Hackathon Season"
           className="w-full"
         />
       </a>
